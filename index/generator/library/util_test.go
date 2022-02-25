7364c6aa6c11df86a33589476200b46696175f8b
Michael Valdron <mvaldron@redhat.com>
2022-02-24 20:05:57 -0500

    Test cases added for util functions. TODO: create test case implementations.

---
+++ index/generator/library/util_test.go
@@ -0,0 +1,31 @@
+package library
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDownloadRemoteStack(t *testing.T) {
+	assert.Fail(t, "Not Implemented.")
+}
+
+func TestDownloadStackFromZipUrl(t *testing.T) {
+	assert.Fail(t, "Not Implemented.")
+}
+
+func TestGetSubDir(t *testing.T) {
+	assert.Fail(t, "Not Implemented.")
+}
+
+func TestCopyFileWithFs(t *testing.T) {
+	assert.Fail(t, "Not Implemented.")
+}
+
+func TestCopyDirWithFS(t *testing.T) {
+	assert.Fail(t, "Not Implemented.")
+}
+
+func TestCleanDir(t *testing.T) {
+	assert.Fail(t, "Not Implemented.")
+}
