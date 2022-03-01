1dde586d73a8ecdca891e88d526fcae6688acfea
Michael Valdron <mvaldron@redhat.com>
2022-03-01 14:28:32 -0500

    TestDownloadFromZipUrl impl added.

--- index/generator/library/util_test.go
+++ index/generator/library/util_test.go
@@ -1,9 +1,14 @@
 package library
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+)
+
+const (
+	zipType string = "application/zip"
 )
 
 func TestDownloadRemoteStack(t *testing.T) {
@@ -11,7 +16,18 @@ func TestDownloadRemoteStack(t *testing.T) {
 }
 
 func TestDownloadStackFromZipUrl(t *testing.T) {
-	assert.Fail(t, "Not Implemented.")
+	bytes, err := DownloadStackFromZipUrl("")
+
+	if err != nil {
+		t.Errorf("Zip download to bytes failed: %v", err)
+	}
+
+	resultantType := http.DetectContentType(bytes)
+
+	if resultantType != zipType {
+		t.Errorf("Content type of download not matching expected. Expected: %s, Actual: %s",
+			zipType, resultantType)
+	}
 }
 
 func TestGetSubDir(t *testing.T) {
